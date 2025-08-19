f8d0cd5cad7aeb32ee0818cf4225a3917cd1e661
NguyenXuanVinh <nongami87@gmail.com>
2025-08-19 21:25:55 +0700

    feat(utils): add utility for merging Tailwind classes

---
+++ src/utils/cn.util.ts
@@ -0,0 +1,10 @@
+import type { ClassValue } from "clsx";
+
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...inputs: ClassValue[]): string => {
+  return twMerge(clsx(inputs));
+};
+
+export default cn;
