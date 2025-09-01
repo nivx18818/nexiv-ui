fdfea93cad8984229d9c61113c3cfa776f54f941
NguyenXuanVinh <nongami87@gmail.com>
2025-09-01 16:23:20 +0700

    feat: add axios and unified HTTP request utility

---
+++ src/utils/http-request.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosError, type AxiosRequestConfig, type Method } from "axios";
+
+const httpRequest = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+});
+
+export type HttpResponse<T = unknown> = {
+  data: T | null;
+  error: { message: string; details: object | null } | null;
+};
+
+export type HttpMethodFn = <T = unknown>(
+  url: string,
+  data?: object,
+  config?: AxiosRequestConfig,
+) => Promise<HttpResponse<T>>;
+
+const request = async <T = unknown>(
+  method: Method,
+  url: string,
+  data?: object,
+  config?: AxiosRequestConfig,
+): Promise<HttpResponse<T>> => {
+  try {
+    const res = await httpRequest.request({
+      method,
+      url,
+      data,
+      ...config,
+    });
+
+    if (res.status == 204) {
+      return { data: { success: true } as T, error: null };
+    }
+
+    const response = res.data;
+    return { data: response.data as T, error: null };
+  } catch (err) {
+    const error = err as AxiosError<{
+      message?: string;
+      details?: object;
+    }>;
+    return {
+      data: null,
+      error: {
+        message: error.response?.data?.message ?? error.message,
+        details: error.response?.data?.details ?? null,
+      },
+    };
+  }
+};
+
+const get: HttpMethodFn = (url, _, config) =>
+  request("get", url, undefined, config);
+const post: HttpMethodFn = (url, data, config) =>
+  request("post", url, data, config);
+const put: HttpMethodFn = (url, data, config) =>
+  request("put", url, data, config);
+const patch: HttpMethodFn = (url, data, config) =>
+  request("patch", url, data, config);
+const del: HttpMethodFn = (url, data, config) =>
+  request("delete", url, data, config);
+
+export default { get, post, put, patch, del };
