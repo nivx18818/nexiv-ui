494ade2726f722cf7c600d9966c2faa88b200b12
NguyenXuanVinh <nongami87@gmail.com>
2025-09-02 07:39:07 +0700

    feat(conversation): add send message function and update param type
    - introduces a new method for sending messages to conversations and updates the ID parameter type from UUID to string for consistency and flexibility.
    - enhances conversation interaction capabilities.

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,12 +1,16 @@
-import httpRequest from "@/utils/http-request.util"
-import type { UUID } from "crypto";
+import httpRequest from "@/utils/http-request.util";
 
 export const create = async () => {
   const res = await httpRequest.post("/c");
   return res;
-}
+};
 
-export const getById = async (id: UUID) => {
+export const getById = async (id: string) => {
   const res = await httpRequest.get(`/c/${id}`);
   return res;
-}
+};
+
+export const sendMessage = async (id: string, message: string) => {
+  const res = await httpRequest.post(`/c/${id}`, { message });
+  return res;
+};
