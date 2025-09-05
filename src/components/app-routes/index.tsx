5a31323b03f4527bf3468827198d64ddd0772f28
NguyenXuanVinh <nongami87@gmail.com>
2025-09-05 21:22:55 +0700

    fix(routes): add catch-all redirect to default path

--- src/components/app-routes/index.tsx
+++ src/components/app-routes/index.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import App from "@/app";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/c/:id" element={<App />} />
+      <Route path="*" element={<Navigate to="/c" />} />
     </Routes>
   );
 }
